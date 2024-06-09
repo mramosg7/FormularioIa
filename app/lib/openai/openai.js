8b9e40a123a87057abc743ececf6b931522ff608
GN0VA <elhombredeacero1000@gmail.com>
2024-06-09 22:52:26 +0200

    openia

--- app/lib/openai/openai.js
+++ app/lib/openai/openai.js
@@ -10,7 +10,7 @@ const openai = new OpenAI({
 });
 
 export async function fetchChat(input) {
-  content="Basado en la descripción, genera un objeto de encuesta con 3 campos: name (cadena) para el formulario, description (cadena) del formulario y un array de preguntas donde cada elemento tiene 2 campos: texto y fieldType, y fieldType puede ser una de estas opciones: RadioGroup, Select y Textarea; y devuélvelo en formato JSON. Para los tipos RadioGroup y Select, también devuelve un array fieldOptions con los campos text y value. Por ejemplo, para los tipos RadioGroup y Select, el array de opciones de campo puede ser [{text: 'Sí', value: 'sí'}, {text: 'No', value: 'no'}] y para los tipos Textarea, el array de opciones de campo puede estar vacío. Por ejemplo, para los tipos Textarea, el array de opciones de campo puede ser []." 
+  const content="Basado en la descripción, genera un objeto de encuesta con 3 campos: name (cadena) para el formulario, description (cadena) del formulario y un array de preguntas donde cada elemento tiene 2 campos: texto y fieldType, y fieldType puede ser una de estas opciones: RadioGroup, Select y Textarea; y devuélvelo en formato JSON. Para los tipos RadioGroup y Select, también devuelve un array fieldOptions con los campos text y value. Por ejemplo, para los tipos RadioGroup y Select, el array de opciones de campo puede ser [{text: 'Sí', value: 'sí'}, {text: 'No', value: 'no'}] y para los tipos Textarea, el array de opciones de campo puede estar vacío. Por ejemplo, para los tipos Textarea, el array de opciones de campo puede ser []." 
   const completion = await openai.chat.completions.create({
     messages: [
       {
